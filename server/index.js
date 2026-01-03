b8b418a7685196f2eaa81aa3e89e1d3f609ce2ef
wladeckialan-collab <wladeckialan@gmail.com>
2026-01-03 01:41:41 +0100

    ORGINAL VERSION

--- server/index.js
+++ server/index.js
@@ -2,22 +2,59 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
-
-// TYMCZASOWE: Zamiast szukać plików, po prostu wyświetlamy tekst
-app.get('/', (req, res) => {
-    res.send('<h1>Serwer działa poprawnie!</h1><p>Jeśli to widzisz, to znaczy, że backend jest gotowy.</p>');
-});
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
+const rooms = {};
+
+// LOGIKA GRY
 io.on('connection', (socket) => {
-    console.log('Ktoś się połączył!');
+    socket.on('createRoom', (username) => {
+        const roomCode = Math.random().toString(36).substring(2, 6).toUpperCase();
+        rooms[roomCode] = { players: [{ id: socket.id, name: username, isHost: true }] };
+        socket.join(roomCode);
+        socket.emit('roomCreated', { roomCode, players: rooms[roomCode].players });
+    });
+
+    socket.on('joinRoom', ({ roomCode, username }) => {
+        const code = roomCode.toUpperCase();
+        if (rooms[code]) {
+            rooms[code].players.push({ id: socket.id, name: username, isHost: false });
+            socket.join(code);
+            io.to(code).emit('updatePlayerList', rooms[code].players);
+            socket.emit('joinSuccess', { roomCode: code });
+        }
+    });
+
+    socket.on('startGame', (roomCode) => {
+        const code = roomCode.toUpperCase();
+        if (rooms[code]) {
+            let roles = ['Mafia', 'Policjant', 'Lekarz', 'Obywatel', 'Obywatel'];
+            const roomPlayers = rooms[code].players;
+            const shuffledRoles = roles.slice(0, roomPlayers.length).sort(() => Math.random() - 0.5);
+            roomPlayers.forEach((p, i) => io.to(p.id).emit('yourRole', shuffledRoles[i]));
+        }
+    });
+});
+
+// OBSŁUGA PLIKÓW (Zabezpieczona)
+const buildPath = path.join(__dirname, '..', 'client', 'build');
+app.use(express.static(buildPath));
+
+app.get('*', (req, res) => {
+    // Sprawdzamy czy plik istnieje, jeśli nie - wysyłamy info testowe
+    res.sendFile(path.join(buildPath, 'index.html'), (err) => {
+        if (err) {
+            res.status(200).send("<h1>Serwer OK!</h1><p>Właśnie buduję frontend, odśwież za chwilę...</p>");
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3001;
