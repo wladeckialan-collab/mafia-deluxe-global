0328b73e009a07d1f49162e998c0ce6a43444242
wladeckialan-collab <wladeckialan@gmail.com>
2026-01-03 02:14:17 +0100

    test

--- server/index.js
+++ server/index.js
@@ -1,63 +1,18 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors');
-const path = require('path');
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: "*", methods: ["GET", "POST"] }
-});
+const io = new Server(server);
 
-const rooms = {};
-
-// LOGIKA GRY
-io.on('connection', (socket) => {
-    socket.on('createRoom', (username) => {
-        const roomCode = Math.random().toString(36).substring(2, 6).toUpperCase();
-        rooms[roomCode] = { players: [{ id: socket.id, name: username, isHost: true }] };
-        socket.join(roomCode);
-        socket.emit('roomCreated', { roomCode, players: rooms[roomCode].players });
-    });
-
-    socket.on('joinRoom', ({ roomCode, username }) => {
-        const code = roomCode.toUpperCase();
-        if (rooms[code]) {
-            rooms[code].players.push({ id: socket.id, name: username, isHost: false });
-            socket.join(code);
-            io.to(code).emit('updatePlayerList', rooms[code].players);
-            socket.emit('joinSuccess', { roomCode: code });
-        }
-    });
-
-    socket.on('startGame', (roomCode) => {
-        const code = roomCode.toUpperCase();
-        if (rooms[code]) {
-            let roles = ['Mafia', 'Policjant', 'Lekarz', 'Obywatel', 'Obywatel'];
-            const roomPlayers = rooms[code].players;
-            const shuffledRoles = roles.slice(0, roomPlayers.length).sort(() => Math.random() - 0.5);
-            roomPlayers.forEach((p, i) => io.to(p.id).emit('yourRole', shuffledRoles[i]));
-        }
-    });
-});
-
-// OBSŁUGA PLIKÓW (Zabezpieczona)
-const buildPath = path.join(__dirname, '..', 'client', 'build');
-app.use(express.static(buildPath));
-
-app.get('*', (req, res) => {
-    // Sprawdzamy czy plik istnieje, jeśli nie - wysyłamy info testowe
-    res.sendFile(path.join(buildPath, 'index.html'), (err) => {
-        if (err) {
-            res.status(200).send("<h1>Serwer OK!</h1><p>Właśnie buduję frontend, odśwież za chwilę...</p>");
-        }
-    });
+// Prosty komunikat, żeby sprawdzić czy serwer w ogóle żyje
+app.get('/test', (req, res) => {
+    res.send('Serwer żyje i odpowiada!');
 });
 
 const PORT = process.env.PORT || 3001;
+
 server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Serwer biega na porcie ${PORT}`);
+    console.log(`SUKCES! Serwer ruszył na porcie ${PORT}`);
 });
