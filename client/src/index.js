cc3389471021ba163b1c8e885a01301020a20812
wladeckialan-collab <wladeckialan@gmail.com>
2026-01-03 02:18:28 +0100

    TEST 2

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+const cors = require('cors');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+app.use(cors());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*", methods: ["GET", "POST"] }
+});
+
+// Ścieżka do plików frontendu
+const buildPath = path.join(__dirname, '..', 'client', 'build');
+app.use(express.static(buildPath));
+
+// Obsługa Socket.io (możesz tu później dopisać logikę gry)
+io.on('connection', (socket) => {
+    console.log('Gracz połączony:', socket.id);
+});
+
+// Serwowanie aplikacji React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'), (err) => {
+        if (err) {
+            res.status(200).send("Serwer działa! Gra się jeszcze ładuje... Odśwież za minutę.");
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Serwer biega na porcie ${PORT}`);
+});
